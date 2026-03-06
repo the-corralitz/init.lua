e71548f2fe89a10cc244f5d11a641002b0f47515
John Doe <sacosa634@gmail.com>
2026-03-06 12:35:19 -0500

    installed tokyonight

--- lua/corralitz/lazy/colorscheme.lua
+++ lua/corralitz/lazy/colorscheme.lua
@@ -1,47 +1,62 @@
 function ColorMyPencils(color)
-    color = color or "gruvbox"
-    vim.cmd.colorscheme(color)
-    if color == "oxocarbon" then
-        vim.opt.background = "dark"
-    end
+	color = color or "gruvbox"
+	vim.cmd.colorscheme(color)
+	if color == "oxocarbon" then
+		vim.opt.background = "dark"
+	end
 
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-    vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"})
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 end
 
 return {
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000 ,
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = true,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = true,
-            })
-            ColorMyPencils()
-        end,
-    },
-    {
-        "nyoom-engineering/oxocarbon.nvim",
-    }
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = false,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "", -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = true,
+			})
+			-- ColorMyPencils()
+		end,
+	},
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+		config = function()
+			-- ColorMyPencils("oxocarbon")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				transparent = true,
+				terminal_colors = true,
+			})
+			ColorMyPencils("tokyonight-storm")
+		end,
+	},
 }
